83e1042e29bcc1a25437d0493028f34bf8d4bc43
Lê Quang Hưng <alatso57@gmail.com>
2022-07-28 01:35:09 +0700

    1001VND

--- API/Extentions/ApplicationServiceExtenntions.cs
+++ API/Extentions/ApplicationServiceExtenntions.cs
@@ -2,14 +2,9 @@
 using API.Helpers;
 using API.Interfaces;
 using API.Services;
-using AutoMapper;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace API.Extentions
 {
@@ -20,6 +15,9 @@ namespace API.Extentions
             services.Configure<CloudinarySettings>(config.GetSection("CloudinarySettings"));
             services.AddScoped<ITokenService, TokenService>();
             services.AddScoped<IPhotoService, PhotoService>();
+            services.AddScoped<ILikesRepository, LikesRepository>();
+            services.AddScoped<IMessageRepository, MessageRepository>();
+            services.AddScoped<LogUserActivity>();
             services.AddScoped<IUserRepository, UserRepository>();
             services.AddAutoMapper(typeof(AutoMapperProfiles).Assembly);
             services
--- client/src/app/members/member-card/member-card.component.ts
+++ client/src/app/members/member-card/member-card.component.ts
@@ -1,3 +1,5 @@
+import { ToastrService } from 'ngx-toastr'
+import { MembersService } from 'src/app/_services/members.service'
 import { Component, Input, OnInit } from '@angular/core'
 import { Router } from '@angular/router'
 import { Member } from 'src/app/_models/member'
@@ -10,7 +12,16 @@ import { AccountService } from 'src/app/_services/account.service'
 })
 export class MemberCardComponent implements OnInit {
   @Input() member: Member
-  constructor() {}
+  constructor(
+    private memberService: MembersService,
+    private toastr: ToastrService,
+  ) {}
 
   ngOnInit(): void {}
+
+  addLike(member: Member) {
+    this.memberService.addLike(member.userName).subscribe(() => {
+      this.toastr.success('You have liked ' + member.knownAs)
+    })
+  }
 }
